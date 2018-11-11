1e11e264ffafc3d1759503aaa43761e17b2a5129
Sapog123 <43165055+Sapog123@users.noreply.github.com>
2018-11-12 01:21:56 +0500

    Update SortingSpec.scala

--- src/test/scala/fintech/homework07/SortingSpec.scala
+++ src/test/scala/fintech/homework07/SortingSpec.scala
@@ -2,5 +2,17 @@ package fintech.homework07
 import org.scalatest.{FlatSpec, Matchers}
 
 class SortingSpec extends FlatSpec with Matchers {
-  ???
+
+  it should "work well quick sort" in {
+    val array = Array(2,7,4,1,3,5,6,8)
+    Sorting.quickSor(array,(a:Int,b: Int)=> a<=b)
+    array should be (Array(1,2,3,4,5,6,7,8))
+  }
+
+  it should "work well merge sort" in {
+    val array = Array(2,7,4,1,3,5,6,8)
+    Sorting.mergeSort(array)
+    array should be (Array(1,2,3,4,5,6,7,8))
+  }
 }
+
