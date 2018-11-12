a433d7d1daaa5d3335e421ed1293ecea898ca7d4
Sapog123 <43165055+Sapog123@users.noreply.github.com>
2018-11-12 19:43:26 +0500

    Update Sorting.scala

--- src/main/scala/fintech/homework07/Sorting.scala
+++ src/main/scala/fintech/homework07/Sorting.scala
@@ -7,19 +7,20 @@ package fintech.homework07
 
 object Sorting {
 
-  def mergeSort[T](array: Array[Int]): Unit = {
-    val temporaryArray = new Array[Int](array.length)
-    MergeSort(array, 0, array.size - 1, temporaryArray )
+  def mergeSort[T](array: collection.mutable.IndexedSeq[T])(implicit smaller: (T,T)=>Boolean): Unit = {
+    val temporaryArray =  array.clone()
+
+    MergeSort(array, 0, array.size - 1, temporaryArray, smaller )
   }
 
-  def Merge[T](array: Array[Int], start: Int, middle: Int, end: Int, temporaryArray: Array[Int]): Unit = {
+  def Merge[T](array:collection.mutable.IndexedSeq[T], start: Int, middle: Int, end: Int, temporaryArray: collection.mutable.Seq[T], smaller : (T,T)=> Boolean): Unit = {
     var leftPtr = start
     var rightPtr = middle + 1
     val length = end - start + 1
     var j = 0
     while(j < length)
      {
-      if (rightPtr > end || (leftPtr <= middle && (array(leftPtr) < array(rightPtr)))) {
+      if (rightPtr > end || (leftPtr <= middle && smaller(array(leftPtr),array(rightPtr)))) {
         temporaryArray(j) = array(leftPtr)
         leftPtr += 1
       }
@@ -37,24 +38,24 @@ object Sorting {
     }
   }
 
-  def MergeSort[T](array: Array[Int], start: Int, end: Int, temporaryArray: Array[Int]): Unit = {
+  def MergeSort[T](array: collection.mutable.IndexedSeq[T], start: Int, end: Int, temporaryArray: collection.mutable.Seq[T], smaller : (T,T)=> Boolean): Unit = {
     if (start == end) return
     val middle = (start + end) / 2
-    MergeSort(array, start, middle, temporaryArray)
-    MergeSort(array, middle + 1, end, temporaryArray)
-    Merge(array, start, middle, end, temporaryArray)
+    MergeSort(array, start, middle, temporaryArray, smaller)
+    MergeSort(array, middle + 1, end, temporaryArray, smaller)
+    Merge(array, start, middle, end, temporaryArray, smaller)
   }
 
 
 
 
-def quickSor[T](array: Array[T], smaller: (T,T)=>Boolean): Unit = {
+def quickSor[T](array: collection.mutable.IndexedSeq[T]) (implicit smaller: (T,T)=>Boolean): Unit = {
 
-  HoareSort(array, 0, array.size - 1, smaller)
+  HoareSort(array, 0, array.size - 1)
 
 }
 
-def HoareSort[T](array: Array[T], start: Int, end: Int, smaller: (T,T)=> Boolean): Unit = {
+def HoareSort[T](array:  collection.mutable.IndexedSeq[T], start: Int, end: Int)(implicit smaller: (T,T)=>Boolean): Unit = {
   if (end == start) return
   val pivot = array(end)
   var storeIndex = start
@@ -62,7 +63,7 @@ def HoareSort[T](array: Array[T], start: Int, end: Int, smaller: (T,T)=> Boolean
   while (i <= end - 1) {
     if (smaller(array(i),pivot)) {
       val t = array(i)
-      array(i) = array(storeIndex)
+      array(i)= array(storeIndex)
       array(storeIndex) = t
       storeIndex += 1
 
@@ -72,10 +73,9 @@ def HoareSort[T](array: Array[T], start: Int, end: Int, smaller: (T,T)=> Boolean
   val n = array(storeIndex)
   array(storeIndex) = array(end)
   array(end) = n
-  if (storeIndex > start) HoareSort(array, start, storeIndex - 1, smaller)
-  if (storeIndex < end) HoareSort(array, storeIndex + 1, end, smaller)
+  if (storeIndex > start) HoareSort(array, start, storeIndex - 1)
+  if (storeIndex < end) HoareSort(array, storeIndex + 1, end)
 }
 
 
 }
-
